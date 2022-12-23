4bd424dbc9ec74fc5159dad19e88db2022741dad
lyaas <lyaastud@gmail.com>
2022-12-23 15:15:57 -0500

    DaraOfficeHours2 created

---
+++ src/dara/DaraOfficeHours2.java
@@ -0,0 +1,10 @@
+package dara;
+
+public class DaraOfficeHours2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("We are going to start a project soon");
+        System.out.println("Flipgrid is waiting");
+    }
+}
