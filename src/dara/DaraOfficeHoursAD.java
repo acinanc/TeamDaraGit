45a6fab0d09de15e27024c5253822becb2286ffe
lyaas <lyaastud@gmail.com>
2022-12-23 20:44:37 -0500

    DaraOfficeHoursAD updated

--- src/dara/DaraOfficeHoursAD.java
+++ src/dara/DaraOfficeHoursAD.java
@@ -1,10 +1,11 @@
 package dara;
 
-public class DaraOfficeHours2 {
+public class DaraOfficeHoursAD {
 
     public static void main(String[] args) {
 
         System.out.println("We are going to start a project soon");
         System.out.println("Flipgrid is waiting");
+        System.out.println("Great Friday Practice");
     }
 }
