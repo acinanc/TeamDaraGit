2d928b3305addfab24dd889343c27f102801312f
yusifrzayev <josephrzayev@gmail.com>
2022-12-23 20:52:31 -0500

    Hi! Created class Yusif.java

--- src/dara/Yusif.java
+++ src/dara/Yusif.java
@@ -4,6 +4,6 @@ public class Yusif {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello! Yusif is here");
+        System.out.println("Hey! Yusif is here");
     }
 }
